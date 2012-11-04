62514abccb346841076b2f22f4887d0af75ba951
Jelena <Jelena@192.168.32.1>
2012-11-04 19:26:44 +0200

    1 step to complete Lists

--- src/lv/Javaguru/Lists/Lect2.java
+++ src/lv/Javaguru/Lists/Lect2.java
@@ -8,45 +8,48 @@ import java.util.Scanner;
 
 /**
  *
- * @author Jelena
+ *
  */
 public class Lect2 {
+
+    public void showMenu() {
+        System.out.println("1: Add Element");
+        System.out.println("2: View Element");
+        System.out.println("3: Exit");
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-       MyList p = new MyList();
-        /*p.add(1, 7);
-        int key=p.getElementValue(1);*/
-        int value;
-       //System.out.println(key);
-        
-        System.out.println("Enter count of elements");
+        Lect2 l = new Lect2();
+        MyList p = new MyList();
         Scanner scan = new Scanner(System.in);
-        int n=scan.nextInt();
-        
-        
-         int flag=0;
-        while(flag!=n)
-        {
-            System.out.println("Enter value of element"+ flag);
-            value = scan.nextInt();
-            p.add(flag, value);
-            flag++;
+        int choose = 0;
+        int value;
+        int number;
+        while (choose != 3) {
+
+            l.showMenu();
+            choose = scan.nextInt();
+            switch (choose) {
+                case 1:
+                    System.out.println("Enter value: ");
+                    value = scan.nextInt();
+                    p.add(value);
+                    break;
+                case 2:
+                    System.out.println("Enter number: ");
+                    number = scan.nextInt();
+                    p.getValue(number);
+                    break;
+                case 3:
+                    choose = 3;
+                    break;
+                default:
+                    choose = 3;
+            }
         }
-        flag=0;
-        
-        
-        int key1;
-                while(flag!=n)
-                {
-                    key1=p.getElementValue(flag);
-                    System.out.println(key1);
-                    flag++;
-                            
-                }
-                
+
     }
 }
