fab81cac15ceed6a0a0c1ee0cd537fbbe12f5a4e
Vitalij <Vitalij@Vitalij-PC>
2012-11-16 00:19:33 +0200

    Lists completed.. need some fixes.. 1) isdigit sum

--- src/lv/Javaguru/TicTacToe/Game.java
+++ src/lv/Javaguru/TicTacToe/Game.java
@@ -9,24 +9,8 @@ package lv.Javaguru.TicTacToe;
  * @author Vitalij
  */
 public class Game {
-    Game game =new Game();
-   private  int[][] field;
-
-    public int[][] getField() {
-        return field;
-    }
-
-    public void setField(int[][] field) {
-        this.field = field;
-    }
-    private int vert;
-    private int hor;
    
-   public void SetMoveOnField(int v, int h)
-   {
-       this.vert=v;
-       this.hor=h;
              
-   }
+   
    
 }
